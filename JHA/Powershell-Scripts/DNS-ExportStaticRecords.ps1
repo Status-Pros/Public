651f7e42d6eef5655734b2461ac8c6d4130ffcdd
Nick C <75757500+SP-Nick@users.noreply.github.com>
2021-06-10 13:46:51 -0700

    Update DNS-ExportStaticRecords.ps1
    modified to display zone names, format table with only necessary fields, export IP correctly

--- JHA/Powershell-Scripts/DNS-ExportStaticRecords.ps1
+++ JHA/Powershell-Scripts/DNS-ExportStaticRecords.ps1
@@ -1,4 +1,6 @@
+clear
 $DC = Read-Host "FQDN of domain controller to connect to"
-$Zone = Read-Host "Zone name"
+Get-DnsServerZone ; write-host "`n"
+$Zone = Read-Host "Zone name to process"
 $SavePath = Read-Host "Path to save CSV"
-Get-DnsServerResourceRecord -ComputerName $DC -ZoneName $Zone | where {-not $_.TimeStamp} | Export-Csv $SavePath -NoTypeInformation
+Get-DnsServerResourceRecord -ComputerName $DC -ZoneName $Zone | where {(-not $_.TimeStamp) -and ($_.Hostname -ne "@") -and ($_.Hostname -ne "_msdcs")} | select HostName, RecordType, @{n='IP';E={$_.recorddata.IPV4Address}} | Export-Csv $SavePath -NoTypeInformation
