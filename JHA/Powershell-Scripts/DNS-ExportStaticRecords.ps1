0e9ae89e549e549ae7d05ab046b5b09c14703420
Nick C <75757500+SP-Nick@users.noreply.github.com>
2021-06-10 16:20:08 -0700

    Update DNS-ExportStaticRecords.ps1
    Updated to include relevant data fields for additional record types

--- JHA/Powershell-Scripts/DNS-ExportStaticRecords.ps1
+++ JHA/Powershell-Scripts/DNS-ExportStaticRecords.ps1
@@ -1,6 +1,6 @@
-clear
+Clear-Host
 $DC = Read-Host "FQDN of domain controller to connect to"
 Get-DnsServerZone ; write-host "`n"
 $Zone = Read-Host "Zone name to process"
 $SavePath = Read-Host "Path to save CSV"
-Get-DnsServerResourceRecord -ComputerName $DC -ZoneName $Zone | where {(-not $_.TimeStamp) -and ($_.Hostname -ne "@") -and ($_.Hostname -ne "_msdcs")} | select HostName, RecordType, @{n='IP';E={$_.recorddata.IPV4Address}} | Export-Csv $SavePath -NoTypeInformation
+Get-DnsServerResourceRecord -ComputerName $DC -ZoneName $Zone | Where-Object {(-not $_.TimeStamp) -and ($_.Hostname -ne "@") -and ($_.Hostname -ne "_msdcs")} | Select-Object HostName, RecordType, @{n='IP';E={$_.recorddata.IPV4Address}}, @{n='HostNameAlias';E={$_.recorddata.HostNameAlias}}, @{n='MailExchange';E={$_.recorddata.MailExchange}}, @{n='DescriptiveText';E={$_.recorddata.DescriptiveText}}, @{n='DomainName';E={$_.recorddata.DomainName}} | Export-Csv $SavePath -NoTypeInformation
